77b101d1a0cd16f44d5d8077052daddbad79a461
0rangeB0y <watwat1243@gmail.com>
2023-11-12 13:27:04 -0500

    rand enemy spawns

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
@@ -21,21 +22,46 @@ public class Enemy : MonoBehaviour
     {
         
         
-        /*if (Input.GetKeyDown(KeyCode.E))
-        {
-            SpawnEnemy();
-        }*/
+        
     }
+
+    //List for current spawn Positions of enemies
+    List<Vector3> spawnedPositions = new List<Vector3>();
 
     public void SpawnEnemy()
     {
-        
-        if (enemyInstance == null)
+        Vector3 spawnPosition;
+        bool positionOccupied;
+
+        if (spawnedPositions.Count < 38)
         {
-            Vector3 spawnPosition = new Vector3(0, 3, 0);
+            do
+            {
+                positionOccupied = false;
+                float randomX = Mathf.Round(UnityEngine.Random.Range(-9.0f, 9.0f));
+                float randomY = Mathf.Round(UnityEngine.Random.Range(3.0f, 4.0f));
+                spawnPosition = new Vector3(randomX, randomY, 0);
+
+                // Check if any enemy has already spawned at this position
+                foreach (Vector3 pos in spawnedPositions)
+                {
+                    if (Vector3.Distance(pos, spawnPosition) < 1) // Check to see if enemy spawns on occupied tile
+                    {
+                        positionOccupied = true;
+                        break;
+                    }
+                }
+            }
+            while (positionOccupied);
 
 
-            enemyInstance = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            GameObject enemyInstance = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+            spawnedPositions.Add(spawnPosition); // Add the spawn position to the list
+        }
+        else
+        {
+            UnityEngine.Debug.Log("Enemy spawns are full");
         }
     }
+
 }
